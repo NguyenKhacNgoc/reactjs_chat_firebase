e5dbd6ae6867252f4c608b6a89c1c2b88ad2fb62
NguyenKhacNgoc <khacngoc6969@gmail.com>
2024-07-16 23:41:37 +0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,203 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  Sidebar,
+  ConversationList,
+  Conversation,
+  Avatar,
+  ChatContainer,
+  ConversationHeader,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+  Search,
+} from "@chatscope/chat-ui-kit-react";
+import { firebase } from "../../findrent/src/firebase"
 
-function App() {
+const ChatScreen = () => {
+  const [messages, setMessages] = useState([]);
+  // senderId và receriverId này tao fix cứng ra đấy.
+  // Mày phải call api để lấy được cái senderId sẽ là user đang login, 
+  // còn cái receiver thì m có thể lấy từ params(cái này tuỳ m)
+  const [sender, setSender] = useState('ngoc')
+  const [receiver, setReceiver] = useState('trung')
+  
+  const fetchData = async () => {
+    try {
+      const messageRef = firebase.firestore().collection('message')
+      //cái này là query của thằng firebase nhé, m có thể tự search mấy cái câu lệnh để truy vấn của nó
+      const query = messageRef.where('sender', 'in', [sender, receiver]).where('receiver', 'in', [sender, receiver]).orderBy('sendAt', 'asc')
+
+      query.onSnapshot((snapshot) => {
+        const messageList = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setMessages(messageList)
+      })
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if (sender !== null && receiver !== null) fetchData()
+  }, [sender, receiver])
+  const handleSendMessage = async (message) => {
+
+    const newMessage = {
+      direction: "outgoing",
+      message: message,
+      sender: 'ngoc',
+      receiver: 'trung',
+      sendAt: firebase.firestore.FieldValue.serverTimestamp()
+    }
+    try {
+      await firebase.firestore().collection('message').add(newMessage)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <MainContainer
+      responsive
+      style={{
+        height: "600px",
+        margin: "0 131px 20px 131px",
+      }}
+    >
+      <Sidebar position="left">
+        <Search placeholder="Search..." />
+        <ConversationList>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Lilly"
+            name="Lilly"
+          >
+            <Avatar
+              name="Lilly"
+              src="https://chatscope.io/storybook/react/assets/lilly-aj6lnGPk.svg"
+              status="available"
+            />
+          </Conversation>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Joe"
+            name="Joe"
+          >
+            <Avatar
+              name="Joe"
+              src="https://chatscope.io/storybook/react/assets/joe-v8Vy3KOS.svg"
+              status="dnd"
+            />
+          </Conversation>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Emily"
+            name="Emily"
+            unreadCnt={3}
+          >
+            <Avatar
+              name="Emily"
+              src="https://chatscope.io/storybook/react/assets/emily-xzL8sDL2.svg"
+              status="available"
+            />
+          </Conversation>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Kai"
+            name="Kai"
+            unreadDot
+          >
+            <Avatar
+              name="Kai"
+              src="https://chatscope.io/storybook/react/assets/kai-5wHRJGb2.svg"
+              status="unavailable"
+            />
+          </Conversation>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Akane"
+            name="Akane"
+          >
+            <Avatar
+              name="Akane"
+              src="https://chatscope.io/storybook/react/assets/akane-MXhWvx63.svg"
+              status="eager"
+            />
+          </Conversation>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Eliot"
+            name="Eliot"
+          >
+            <Avatar
+              name="Eliot"
+              src="https://chatscope.io/storybook/react/assets/eliot-JNkqSAth.svg"
+              status="away"
+            />
+          </Conversation>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Zoe"
+            name="Zoe"
+          >
+            <Avatar
+              name="Zoe"
+              src="https://chatscope.io/storybook/react/assets/zoe-E7ZdmXF0.svg"
+              status="dnd"
+            />
+          </Conversation>
+          <Conversation
+            info="Yes i can do it for you"
+            lastSenderName="Patrik"
+            name="Patrik"
+          >
+            <Avatar
+              name="Patrik"
+              src="https://chatscope.io/storybook/react/assets/patrik-yC7svbAR.svg"
+              status="invisible"
+            />
+          </Conversation>
+        </ConversationList>
+      </Sidebar>
+      <ChatContainer>
+        <ConversationHeader>
+          <ConversationHeader.Back />
+          <Avatar
+            name="Zoe"
+            src="https://chatscope.io/storybook/react/assets/zoe-E7ZdmXF0.svg"
+          />
+          <ConversationHeader.Content
+            info="Active 10 mins ago"
+            userName="Zoe"
+          />
 
-export default App;
+        </ConversationHeader>
+        <MessageList
+          typingIndicator={<TypingIndicator content="Zoe is typing" />}
+        >
+          {messages.map((msg, index) => (
+            <Message key={index} model={msg}>
+              {/* {msg.direction === "incoming" && (
+                <Avatar
+                  name={msg.sender}
+                  src="https://chatscope.io/storybook/react/assets/zoe-E7ZdmXF0.svg"
+                />
+              )} */}
+            </Message>
+          ))}
+        </MessageList>
+        <MessageInput
+          placeholder="Type message here"
+          onSend={(m) => handleSendMessage(m)}
+        />
+      </ChatContainer>
+    </MainContainer>
+  );
+};
+
+export default ChatScreen;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyDb-K2zSMmt5FuCQdCf7DA3sc0dhOLrkUo",
+  authDomain: "rentfind-1bc87.firebaseapp.com",
+  projectId: "rentfind-1bc87",
+  storageBucket: "rentfind-1bc87.appspot.com",
+  messagingSenderId: "994479302377",
+  appId: "1:994479302377:web:ad72a93a30227dcc50b278",
+  measurementId: "G-ZVK9P36DYL"
+}
+if(!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+export {firebase}
